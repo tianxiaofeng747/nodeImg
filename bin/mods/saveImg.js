0d4eda577e597a5b29fd0b6efc5e2bfff3d99a23
smallkugua <tianxiaofeng747@163.com>
2018-07-20 20:54:32 +0800

    22323

--- bin/mods/saveImg.js
+++ bin/mods/saveImg.js
@@ -5,55 +5,56 @@ let Ut = {
 	/**
 	 * 下载网络图片
 	 */
-    downImg(opts = {}) {
-		let url = opts.url.split('/').pop();
+	downImg(opts = {}) {
+		let url = new Date().getTime() + opts.url.split('/').pop();
 		let dirPath = 'public/download/';
 		if (!fs.existsSync(dirPath)) {
 			fs.mkdirSync(dirPath);
 		}
 		return new Promise((resolve, reject) => {
-			request
-				.get(opts)
-				.on('response', (response) => {
+			try {
+				request.get(opts).on('response', (response) => {
 					console.log("正在请求:", opts.url)
-				})
-				.pipe(fs.createWriteStream(dirPath + url))
-				.on("error", (e) => {
+				}).pipe(fs.createWriteStream(dirPath + url)).on("error", (e) => {
 					console.log("下载错误", e);
 					reject({
-                        type: 'detail',
-                        msg: opts.url + '下载错误'
-                    });
-				})
-				.on("finish", () => {
-                    db.update(opts);
+						type: 'detail',
+						msg: opts.url + '下载错误'
+					});
+				}).on("finish", () => {
+					db.update(opts);
 					resolve({
 						type: 'detail',
 						msg: opts.url + '下载完成'
 					});
-				})
-				.on("close", () => {
+				}).on("close", () => {
 					//console.log("关闭");
 				})
+			} catch (e) {
+				reject({
+					type: 'detail',
+					msg: opts.url + '下载错误'
+				})
+			}
 
 		})
 	},
-    saveUrl(list){
-        return new Promise(function (resolve,reject) {
-            db.insert(list).then((result) => {
-                if (result) {
-                    reject();
-                    console.log('保存失败');
-                } else {
-                    console.log('保存成功');
-                    resolve();
-                }
+	saveUrl(list){
+		return new Promise(function (resolve, reject) {
+			db.insert(list).then((result) => {
+				if (result) {
+					reject();
+					console.log('保存失败');
+				} else {
+					console.log('保存成功');
+					resolve();
+				}
 
-            }).catch((err) => {
-                reject();
-            });
-        })
+			}).catch((err) => {
+				reject();
+			});
+		})
 
-    }
+	}
 };
 module.exports = Ut;
--- bin/mods/socket.js
+++ bin/mods/socket.js
@@ -6,7 +6,7 @@ const superagent = require('superagent');
 const Throttle = require('superagent-throttle')
 const fs = require('fs');
 let Ut = require('./saveImg');
-
+let db = require('../../db/db.js');
 let throttle = new Throttle({
     active: true,     // set false to pause queue
     rate: 6,          // how many requests can be sent every `ratePer`
@@ -32,6 +32,7 @@ let Page = {
     IO: null,
     pageNum: 0,
     imgTotal: 0,
+	URL: 'http://www.rosimm8.com',
     imgList: [],
     /*
     * 传输类型
@@ -45,55 +46,72 @@ let Page = {
         Page.IO.emit('progress', type);
     },
     init(io, msg){
-        const reptileUrl = "http://www.meisiguan.cc/?s=";
+		const reptileUrl = "/rosimm/";
         this.IO = io;
-        if (!msg) {
-            this.sendClient({
-                type: 'err',
-                msg: '请输入关键词'
-            })
-        } else {
-            superagent.get(reptileUrl + msg).timeout({
-                response: 5000,  // Wait 5 seconds for the server to start sending,
-                deadline: 60000, // but allow 1 minute for the file to finish loading.
-            }).use(throttle.plugin()).end((err, res) => {
-                // 抛错拦截
-                if (err) {
-                    if(err.timeout){
-                        this.sendClient({
-                            type: 'err',
-                            msg: '请求超时'
-                        });
-                    }
-                    console.log(err)
-                    return;
-                }
-                let $ = cheerio.load(res.text);
-                let data = [];
-                // 下面就是和jQuery一样获取元素，遍历，组装我们需要数据，添加到数组里面
-                $('#index_ajax_list li').each(function (i, elem) {
-                    let _this = $(elem);
-                    data.push({
-                        img: _this.find('img').attr('src'),
-                        title: _this.find('img').attr('alt'),
-                        href: _this.find('a').attr('href')
-                    });
-                });
-                this.pageNum = data.length;
-                this.sendClient({
-                    type: 'page',
-                    msg: this.pageNum
-                });
-                this.getSonPage(data);
-            });
-        }
+		db.search({
+			isDownload: undefined
+		}).then(list =>{
+			this.imgList = list.filter(item =>{
+				return /http/g.test(item.url);
+			});
+			if(this.imgList.length){
+				this.imgTotal  = this.imgList.length;
+				/*this.imgList.unshift({
+					id: 'testadd',
+					url: 'http://www.rosimm8.com//uploads/allimg/181706/0211.jpg'
+				})*/
+				this.download(true);
+			}else{
+				if (!msg) {
+					this.sendClient({
+						type: 'err',
+						msg: '请输入关键词'
+					})
+				} else {
+					superagent.get(this.URL + reptileUrl).timeout({
+						response: 5000,  // Wait 5 seconds for the server to start sending,
+						deadline: 60000, // but allow 1 minute for the file to finish loading.
+					}).use(throttle.plugin()).end((err, res) => {
+						// 抛错拦截
+						if (err) {
+							if(err.timeout){
+								this.sendClient({
+									type: 'err',
+									msg: '请求超时'
+								});
+							}
+							console.log(err)
+							return;
+						}
+						let $ = cheerio.load(res.text);
+						let data = [];
+						// 下面就是和jQuery一样获取元素，遍历，组装我们需要数据，添加到数组里面
+						$('a.thumbnail').each(function (i, elem) {
+							let _this = $(elem);
+							data.push({
+								title: _this.find('img').attr('alt'),
+								href: _this.attr('href'),
+								pageIndex: 1
+							});
+						});
+						this.pageNum = data.length;
+						this.sendClient({
+							type: 'page',
+							msg: this.pageNum
+						});
+						//let tests = data.slice(0,1);
+						this.getSonPage(data);
+					});
+				}
+			}
+		});
     },
     clearData(){
         this.pageNum = 0;
         this.imgList = [];
         //this.IO.close();
     },
-    download(){
+    download(ignoreSave){
         let self = this,
             finishNum = 0,
             done = () => {
@@ -132,17 +150,19 @@ let Page = {
             type: 'detail',
             msg: '开始下载图片'
         });
-        Ut.saveUrl(this.imgList).then(result =>{
-            downloadSigle();
-        });
+		if(ignoreSave){
+			downloadSigle();
+		}else{
+			Ut.saveUrl(this.imgList).then(result =>{
+				downloadSigle();
+			});
+		}
     },
     getSonPage(list){
         let self = this,
             finishNum = 0,
             done = () => {
-                if (finishNum == self.pageNum) {
-                    self.download.call(self);
-                }
+				self.download.call(self);
             };
         if(!list.length){
             self.sendClient({
@@ -156,42 +176,60 @@ let Page = {
             console.log('执行完成');
             return;
         }
-        list.forEach((item, index) => {
-            let url = item.href;
-            superagent.get(url).timeout({
-                response: 8000,
-                deadline: 60000,
-            }).use(throttle.plugin()).end((err, res) => {
-                if (err) {
-                    if(err.timeout){
-                        this.sendClient({
-                            type: 'detail',
-                            msg: `请求${url}超时`
-                        });
-                    }
-                    return ;
-                }
-                let $ = cheerio.load(res.text),
-                    p = $('.content_left p');
-
-                $('.content_left p').each((i, elem) => {
-                    let _this = $(elem),
-                        obj = {
-                            url: _this.find('img').attr('src'),
-                            id: new Date().getTime() + ''
-                        };
-                    obj.url && self.imgList.push(obj);
-                });
-                finishNum++;
-                self.imgTotal = this.imgList.length;
-                self.sendClient({
-                    type: 'img',
-                    total: self.imgTotal,
-                    num: 0
-                });
-                done();
-            });
-        });
-    }
+		let exec = () =>{
+			if(list.length){
+				let item = list.shift();
+				finishNum++;
+				this.getPagination(item, exec);
+			}else{
+				done();
+			}
+		};
+		exec();
+    },
+	getPagination(item,done){
+		let self = this,
+			url = item.href;
+		superagent.get(url).timeout({
+			response: 8000,
+			deadline: 60000,
+		}).use(throttle.plugin()).end((err, res) => {
+			if (err) {
+				if(err.timeout){
+					this.sendClient({
+						type: 'detail',
+						msg: `请求${url}超时`
+					});
+				}
+				return ;
+			}
+			let $ = cheerio.load(res.text),
+				imgs = $('.article-content img');
+			let total = $('.pageinfo strong').text();
+			total = total.match(/\d+/)[0];
+			item.total = total;
+			imgs.each((i, elem) => {
+				let _this = $(elem),
+					obj = {
+						url: self.URL + _this.attr('src'),
+						id: new Date().getTime() + ''
+					};
+				obj.url && self.imgList.push(obj);
+			});
+			self.imgTotal = self.imgList.length;
+			self.sendClient({
+				type: 'img',
+				total: self.imgTotal,
+				num: 0
+			});
+			if(item.pageIndex < item.total){
+				item.pageIndex++;
+				item.href = item.href.replace(/(\d+)(_?\d*).html/,'$1'+ `_${item.pageIndex}.html`);
+				this.getPagination(item, done);
+			}else{
+				done()
+			}
+		});
+	}
 };
 module.exports = Page;
